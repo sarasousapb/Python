6434a871c1d586d802a091f43d6139d6fb82e3cb
Sara Sousa <101675506+sarasousapb@users.noreply.github.com>
2022-03-27 10:04:40 -0300

    Create exe13.py
    13. Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.

---
+++ Estrutura de Decisão/exe13.py
@@ -0,0 +1,17 @@
+dia = int(input('Sendo: 1 - Domingo, 2 - Segunda-feira, 3 - Terça-feira, 4 - Quarta-feira, 5 - Quinta-feira, 6 - Sexta-feira e 7 - Sábado \n Insira um número referente ao dia da semana: '))
+if dia == 1:
+	print('Domingo')
+elif dia == 2:
+	print('Segunda-feira')
+elif dia == 3:
+	print('Terça-feira')
+elif dia == 4:
+	print('Quarta-feira')
+elif dia == 5:
+	print('Quinta-feira')
+elif dia == 6:
+	print('Sexta-feira')
+elif dia == 7:
+	print('Sábado')
+else:
+	print('Valor inválido')
