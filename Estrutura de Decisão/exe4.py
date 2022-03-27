63e275a5350486ca852f8f04163835440dfd4154
Sara Sousa <101675506+sarasousapb@users.noreply.github.com>
2022-03-27 09:49:23 -0300

    Create exe4.py
    4. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.

---
+++ Estrutura de Decisão/exe4.py
@@ -0,0 +1,6 @@
+letra = input('Insira uma letra: ')
+vogais = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+if letra in vogais:
+	print('A letra inserida é uma vogal.')
+else:
+	print('A letra inserida é uma consoante.')
