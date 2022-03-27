f25f0986371708ca4e0b742d69baeb3f32c5bccc
Sara Sousa <101675506+sarasousapb@users.noreply.github.com>
2022-03-27 09:40:33 -0300

    Delete exercise6.py

--- exercise6.py
+++
@@ -1,4 +0,0 @@
-import math
-raio = float(input('Determine o raio da figura circular: '))
-print('O comprimento da circunferência é: ',round(2*raio*(math.pi), 2))
-print('A área da circunferência é: ', round((math.pi)*(raio**2), 2))
