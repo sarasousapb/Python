32b5d48257f7b69fb1e7d8b0e92964bf520f9446
Sara Sousa <101675506+sarasousapb@users.noreply.github.com>
2022-03-27 09:48:45 -0300

    Create exe3.py
    3. Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.

---
+++ Estrutura de Decisão/exe3.py
@@ -0,0 +1,7 @@
+sexo = input('Escolha o sexo de nascimento, entre F - Feminino, ou M - Masculino: ')
+if (sexo == ('F')) or (sexo == ('f')):
+	print('F - Feminino.')
+elif (sexo == ('M')) or (sexo == ('m')):
+	print('M - Masculino.')
+else:
+  print('Sexo inválido!')
