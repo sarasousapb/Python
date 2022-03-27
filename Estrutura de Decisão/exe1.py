39d046a9ce1871a40d90883d1ae41147ea53afac
Sara Sousa <101675506+sarasousapb@users.noreply.github.com>
2022-03-27 09:45:50 -0300

    Create exe1.py
    1. Faça um Programa que peça dois números e imprima o maior deles.

---
+++ Estrutura de Decisão/exe1.py
@@ -0,0 +1,6 @@
+numb1 = float(input('Insira o valor do primeiro número: '))
+numb2 = float(input('Insira o valor do segundo número: '))
+if numb1 > numb2:
+	print('O maior número é o primeiro valor inserido, ou seja, ', numb1)
+else:
+	print('O maior número é o segundo valor inserido, ou seja, ', numb2)
