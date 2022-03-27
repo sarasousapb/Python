9e1d329eae3de01caf5ee60bd3da08ae517abd70
Sara Sousa <101675506+sarasousapb@users.noreply.github.com>
2022-03-27 09:39:29 -0300

    Delete exercise8.py

--- exercise8.py
+++
@@ -1,3 +0,0 @@
-valor = float(input('Insira o valor recebido por hora trabalhada: '))
-horas = int(input('Insira quantas horas foram trabalhadas: '))
-print('O valor a ser recebido será de R$', round(valor*horas, 2))
