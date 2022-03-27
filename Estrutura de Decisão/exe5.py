977f65efb3bc864cc404a6cc9e8c35caf60782e5
Sara Sousa <101675506+sarasousapb@users.noreply.github.com>
2022-03-27 09:50:06 -0300

    Create exe5.py
    5. Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
    - A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
    - A mensagem "Reprovado", se a média for menor do que sete;
    - A mensagem "Aprovado com Distinção", se a média for igual a dez.

---
+++ Estrutura de Decisão/exe5.py
@@ -0,0 +1,8 @@
+media = float(input('Insira a média obtida: '))
+if media == 10:
+	print('Aluno aprovado com distinção!')
+else:
+	if media >= 7:
+	 	print('Aluno aprovado!')
+	if media < 7:
+	 	print('Aluno reprovado!')
